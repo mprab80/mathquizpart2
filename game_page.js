62d4275e749bb66f7e0f022d7f0e1370b2685c41
Prabu Murugan <55397410+mprab80@users.noreply.github.com>
2022-06-11 14:31:14 -0400

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+p1name=localStorage.getItem("Player 1 Name");
+p2name=localStorage.getItem("Player 2 Name");
+p1score=0;
+p2score=0;
+document.getElementById("p1name").innerHTML=p1name+":";
+document.getElementById("p2name").innerHTML=p2name+":";
+document.getElementById("p1score").innerHTML=p1score;
+document.getElementById("p2score").innerHTML=p2score;
+document.getElementById("playerquestion").innerHTML="Question Turn: "+p1name;
+document.getElementById("playeranswer").innerHTML="Answer Turn: "+p2name;
